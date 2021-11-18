b05dc3fea01156a95ca8726c7cbe442cafe70bba
Grady Roberts <gnr7aj@virginia.edu>
2021-11-18 14:50:43 -0500

    Add /balance command

---
+++ commands/balance.js
@@ -0,0 +1,20 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { User } = require('../db-objects');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('balance')
+        .setDescription('View your Bepcoin balance and daily login streak!'),
+    async execute(interaction) {
+        const uid = interaction.user.id;
+        const uname = interaction.user.username;
+        const [user, _created] = await User.findOrCreate({
+            where: { userId: uid },
+            attributes: ['balance', 'loginStreak']
+        });
+        await interaction.reply({
+            content: `${uname}, your current BepCoin balance is **${user.balance}** :coin: and you have a login streak of **${user.loginStreak} days**!\n\n*Join voice call at least once per day to earn more coins and build your streak*`,
+            ephemeral: true
+        });
+    },
+};
